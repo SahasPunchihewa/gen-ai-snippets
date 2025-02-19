f8bca1bd5ac116cc56d34f0c68d7671fe39b6fc3
Sahas <sahasmcg2995@gmail.com>
2025-02-19 19:53:41 +0530

    Organiozed code structure

---
+++ in_memory_db.py
@@ -0,0 +1,311 @@
+players = [
+    {
+        "id": 1,
+        "name": "Juan Soto",
+        "age": 26,
+        "contract_start_date": "2024-12-11",
+        "contract_end_date": "2039-12-10",
+        "team": "New York Mets",
+        "country": "Dominican Republic",
+        "salary": "51 million per year"
+    },
+    {
+        "id": 2,
+        "name": "Brent Rooker",
+        "age": 30,
+        "contract_start_date": "2025-01-07",
+        "contract_end_date": "2030-01-06",
+        "team": "Oakland Athletics",
+        "country": "United States",
+        "salary": "12 million per year"
+    },
+    {
+        "id": 3,
+        "name": "Blake Snell",
+        "age": 32,
+        "contract_start_date": "2025-01-01",
+        "contract_end_date": "2029-12-31",
+        "team": "Los Angeles Dodgers",
+        "country": "United States",
+        "salary": "36.4 million per year"
+    },
+    {
+        "id": 4,
+        "name": "Paul Goldschmidt",
+        "age": 37,
+        "contract_start_date": "2024-12-30",
+        "contract_end_date": "2025-12-29",
+        "team": "New York Yankees",
+        "country": "United States",
+        "salary": "12.5 million per year"
+    },
+    {
+        "id": 5,
+        "name": "Reynaldo López",
+        "age": 31,
+        "contract_start_date": "2023-11-20",
+        "contract_end_date": "2026-11-19",
+        "team": "Atlanta Braves",
+        "country": "Dominican Republic",
+        "salary": "8 million in 2025, 14 million in 2026"
+    },
+    {
+        "id": 6,
+        "name": "Max Fried",
+        "age": 30,
+        "contract_start_date": "2024-12-17",
+        "contract_end_date": "2032-12-16",
+        "team": "New York Yankees",
+        "country": "United States",
+        "salary": "27.25 million per year"
+    },
+    {
+        "id": 7,
+        "name": "Teoscar Hernández",
+        "age": 32,
+        "contract_start_date": "2025-01-01",
+        "contract_end_date": "2028-12-31",
+        "team": "Los Angeles Dodgers",
+        "country": "Dominican Republic",
+        "salary": "22 million per year"
+    },
+    {
+        "id": 8,
+        "name": "Luis Severino",
+        "age": 30,
+        "contract_start_date": "2025-01-01",
+        "contract_end_date": "2027-12-31",
+        "team": "Oakland Athletics",
+        "country": "Dominican Republic",
+        "salary": "22.33 million per year"
+    },
+    {
+        "id": 9,
+        "name": "Sean Manaea",
+        "age": 32,
+        "contract_start_date": "2024-12-27",
+        "contract_end_date": "2027-12-26",
+        "team": "New York Mets",
+        "country": "United States",
+        "salary": "25 million per year"
+    },
+    {
+        "id": 10,
+        "name": "Nathan Eovaldi",
+        "age": 34,
+        "contract_start_date": "2024-12-12",
+        "contract_end_date": "2027-12-11",
+        "team": "Texas Rangers",
+        "country": "United States",
+        "salary": "25 million per year"
+    }
+]
+
+landmarks = [
+    {
+        "name": "Eiffel Tower",
+        "location": "Paris, France",
+        "year_completed": 1889,
+        "description": "An iron lattice tower designed by Gustave Eiffel, a global symbol of France."
+    },
+    {
+        "name": "Great Wall of China",
+        "location": "China",
+        "year_completed": "7th century BC - 16th century AD",
+        "description": "A series of fortifications built to protect China from invasions, stretching over 13,000 miles."
+    },
+    {
+        "name": "Statue of Liberty",
+        "location": "New York City, USA",
+        "year_completed": 1886,
+        "description": "A colossal neoclassical statue gifted by France, symbolizing freedom and democracy."
+    },
+    {
+        "name": "Machu Picchu",
+        "location": "Peru",
+        "year_completed": "15th century",
+        "description": "An ancient Incan city set high in the Andes Mountains, known for its sophisticated dry-stone construction."
+    },
+    {
+        "name": "Taj Mahal",
+        "location": "Agra, India",
+        "year_completed": 1648,
+        "description": "A white marble mausoleum built by Mughal Emperor Shah Jahan in memory of his wife Mumtaz Mahal."
+    },
+    {
+        "name": "Colosseum",
+        "location": "Rome, Italy",
+        "year_completed": '80 AD',
+        "description": "An ancient Roman amphitheater known for its gladiator battles, seating up to 50,000 spectators."
+    },
+    {
+        "name": "Pyramids of Giza",
+        "location": "Giza, Egypt",
+        "year_completed": "c. 2600 BC",
+        "description": "A group of massive pyramids, including the Great Pyramid, built as tombs for Egyptian pharaohs."
+    },
+    {
+        "name": "Sydney Opera House",
+        "location": "Sydney, Australia",
+        "year_completed": 1973,
+        "description": "A UNESCO World Heritage Site known for its sail-like architectural design, hosting world-class performances."
+    },
+    {
+        "name": "Christ the Redeemer",
+        "location": "Rio de Janeiro, Brazil",
+        "year_completed": 1931,
+        "description": "A towering statue of Jesus Christ standing atop Mount Corcovado, symbolizing peace and Christianity."
+    },
+    {
+        "name": "Petra",
+        "location": "Jordan",
+        "year_completed": "c. 5th century BC",
+        "description": "An ancient rock-cut city, also known as the 'Rose City,' famous for its intricate architecture and water conduit system."
+    }
+]
+
+trains = [
+    {
+        "class": "M1",
+        "model_number": None,
+        "manufacture_year": "1952",
+        "manufacturer": "Brush Bagnall Traction",
+        "engine_model": "Mirlees JS12VT",
+        "engine_country": "UK",
+        "axle_arrangement": "A1A-A1A",
+        "max_power_hp": 1000,
+        "weight_tons": 88,
+        "height_mm": None,
+        "length_mm": None
+    },
+    {
+        "class": "M2",
+        "model_number": "G12",
+        "manufacture_year": "1954–1966",
+        "manufacturer": "General Motors Diesel (Canada) and Electro-Motive Division (USA)",
+        "engine_model": "EMD 567C",
+        "engine_country": "USA",
+        "axle_arrangement": "A1A-A1A",
+        "max_power_hp": 1400,
+        "weight_tons": None,
+        "height_mm": None,
+        "length_mm": None
+    },
+    {
+        "class": "M3",
+        "model_number": None,
+        "manufacture_year": "1956–1958",
+        "manufacturer": "Sri Lanka Railways",
+        "engine_model": "English Electric Willans H6",
+        "engine_country": "UK",
+        "axle_arrangement": "Bo-Bo",
+        "max_power_hp": 360,
+        "weight_tons": 47,
+        "height_mm": None,
+        "length_mm": None
+    },
+    {
+        "class": "M4",
+        "model_number": "MX-620",
+        "manufacture_year": 1975,
+        "manufacturer": "Montreal Locomotive Works",
+        "engine_model": None,
+        "engine_country": "Canada",
+        "axle_arrangement": "Co-Co",
+        "max_power_hp": 1750,
+        "weight_tons": None,
+        "height_mm": None,
+        "length_mm": None
+    },
+    {
+        "class": "M5",
+        "model_number": None,
+        "manufacture_year": "1979",
+        "manufacturer": "Hitachi",
+        "engine_model": None,
+        "engine_country": "Japan",
+        "axle_arrangement": "Bo-Bo",
+        "max_power_hp": 1640,
+        "weight_tons": None,
+        "height_mm": None,
+        "length_mm": None
+    },
+    {
+        "class": "M6",
+        "model_number": "G22CW",
+        "manufacture_year": "1979–1980",
+        "manufacturer": "Thyssen-Henschel",
+        "engine_model": "EMD 645E",
+        "engine_country": "USA",
+        "axle_arrangement": "A1A-A1A",
+        "max_power_hp": 1650,
+        "weight_tons": 87,
+        "height_mm": 4113,
+        "length_mm": 16459
+    },
+    {
+        "class": "M7",
+        "model_number": None,
+        "manufacture_year": "1981",
+        "manufacturer": "Brush Traction",
+        "engine_model": "GM 8-645E",
+        "engine_country": "USA",
+        "axle_arrangement": "Bo-Bo",
+        "max_power_hp": 994,
+        "weight_tons": 66,
+        "height_mm": None,
+        "length_mm": 12800
+    },
+    {
+        "class": "M8",
+        "model_number": "WDM-2",
+        "manufacture_year": "1995",
+        "manufacturer": "Banaras Locomotive Works",
+        "engine_model": "ALCO 251-B",
+        "engine_country": "USA",
+        "axle_arrangement": "Co-Co",
+        "max_power_hp": 2800,
+        "weight_tons": 112.8,
+        "height_mm": 4185,
+        "length_mm": 17120
+    },
+    {
+        "class": "M9",
+        "model_number": None,
+        "manufacture_year": "2000",
+        "manufacturer": "Alstom",
+        "engine_model": "AD32C",
+        "engine_country": "France",
+        "axle_arrangement": "Co-Co",
+        "max_power_hp": 1800,
+        "weight_tons": None,
+        "height_mm": None,
+        "length_mm": None
+    },
+    {
+        "class": "M10",
+        "model_number": "WDM-3D",
+        "manufacture_year": "2012",
+        "manufacturer": "Banaras Locomotive Works",
+        "engine_model": "ALCO 251 B-16",
+        "engine_country": "USA",
+        "axle_arrangement": "Co-Co",
+        "max_power_hp": 2300,
+        "weight_tons": 117,
+        "height_mm": 4077,
+        "length_mm": 18650
+    },
+    {
+        "class": "M11",
+        "model_number": None,
+        "manufacture_year": "2018–2019",
+        "manufacturer": "Banaras Locomotive Works",
+        "engine_model": "EMD 12-710G3B-ES",
+        "engine_country": "USA",
+        "axle_arrangement": "Co-Co",
+        "max_power_hp": 3200,
+        "weight_tons": 120,
+        "height_mm": 4165,
+        "length_mm": 21000
+    }
+]
